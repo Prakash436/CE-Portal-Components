5fff170cc332ef02d8cebf2d97a7b8eca92ed3ed
mohit <mohitsharma55337@gmail.com>
2023-09-30 01:49:40 +0530

    interaction disabled and faster transitions

--- HeroSectionsec/Hero-section-map/script.js
+++ HeroSectionsec/Hero-section-map/script.js
@@ -11,31 +11,63 @@ var Lucky = 0.75;
 console.log(scrollmap);
 
 document.addEventListener('DOMContentLoaded', function () {
+    var scrollmap = document.querySelector('.main-hero-section-map');
+    var originLeft = scrollmap.scrollWidth / 2 - window.innerWidth / 2;
+    var originTop = scrollmap.scrollHeight / 2 - window.innerHeight / 2;
+
+    console.log('Origin Left:', originLeft);
+    console.log('Origin Top:', originTop);
+
+    scrollmap.scrollLeft = originLeft;
+    scrollmap.scrollTop = originTop;
+
+    // Delay execution for 3 seconds (3000 milliseconds) before starting the smooth scrolling
     setTimeout(function () {
-        var scrollmap = document.querySelector('.main-hero-section-map');
-        var scrollLeft = scrollmap.scrollWidth / 2 - window.innerWidth / 2;
-        var scrollTop = scrollmap.scrollHeight / 2 - window.innerHeight / 2;
-        console.log(scrollLeft);
-        console.log(scrollTop);
+        // Define destination points with multipliers
+        var destinations = [
+            { leftMultiplier: 1, topMultiplier:1 },//dont remove it otherwise jerky initial transition
+            //Add random destinations
+            //chose multipliers carefully otherwise you'll end up in water
+            { leftMultiplier: (.8 * Math.random() + 0.2), topMultiplier: Math.min((3* Math.random()),1.7) },
+            { leftMultiplier: 0.7 * Math.random(), topMultiplier: Math.min(2 * Math.random(),1.8) },
+             // final destination
+            { leftMultiplier: Luckx, topMultiplier: Lucky },
+           
+        ];
 
-        // Ensure the element exists
-        if (scrollmap) {
-            smoothScroll(scrollmap, scrollLeft, scrollTop, 2000);
+        // Function to calculate the duration based on distance
+        function calculateDuration(destination, originLeft, originTop) {
+            var destinationLeft = destination.leftMultiplier * originLeft;
+            var destinationTop = destination.topMultiplier * originTop;
+            var distance = Math.sqrt(Math.pow(destinationLeft - originLeft, 2) + Math.pow(destinationTop - originTop, 2));
+            return Math.max(1000, distance * 2); // Minimum duration of 1000ms (1 second)
         }
-    }, 2000); // Delay
 
-    setTimeout(function () {
-        var scrollmap = document.querySelector('.main-hero-section-map');
-        var scrollLeft = scrollmap.scrollWidth / 2 - window.innerWidth / 2;
-        var scrollTop = scrollmap.scrollHeight / 2 - window.innerHeight / 2;
+        // Function to perform smooth scrolling with delay
+        function performSmoothScroll(destinationIndex) {
+            if (destinationIndex < destinations.length) {
+                var destination = destinations[destinationIndex];
+                var destinationLeft = destination.leftMultiplier * originLeft;
+                var destinationTop = destination.topMultiplier * originTop;
 
-        if (scrollmap) {
-            smoothScroll(scrollmap, scrollLeft * 0.9, scrollTop * 0.75, 4000);
+                // Shorter initial duration for the first transition
+                var duration = destinationIndex === 0 ? 1000 : calculateDuration(destination, destinations[destinationIndex - 1].leftMultiplier * originLeft, destinations[destinationIndex - 1].topMultiplier * originTop);
+
+                setTimeout(function () {
+                    smoothScroll(scrollmap, destinationLeft, destinationTop, duration, function () {
+                        // Recursive call for the next destination
+                        performSmoothScroll(destinationIndex + 1);
+                    });
+                }, 2000); // 2-second delay before starting the smooth scrolling
+            }
         }
-    }, 4000);
+
+        // Start smooth scrolling
+        performSmoothScroll(0);
+    }, 1000); // Delay for 3 seconds after DOM load
 });
 
-function smoothScroll(element, toLeft, toTop, duration) {
+function smoothScroll(element, toLeft, toTop, duration, callback) {
     var start = {
         scrollLeft: element.scrollLeft,
         scrollTop: element.scrollTop,
@@ -50,57 +82,24 @@ function smoothScroll(element, toLeft, toTop, duration) {
         var elapsedTime = currentTime - startTime;
         var progress = Math.min(elapsedTime / duration, 1);
 
+        // Apply easing effect (slower ease-in-out)
+        progress = easeInOutCubic(progress);
+
         element.scrollLeft = start.scrollLeft + change.scrollLeft * progress;
         element.scrollTop = start.scrollTop + change.scrollTop * progress;
 
         if (progress < 1) {
             requestAnimationFrame(animateScroll);
+        } else {
+            if (callback) callback(); // Call the callback when scrolling is done
         }
     }
 
     requestAnimationFrame(animateScroll);
 }
-//Check whichever works better 
 
-// let dropdownicon = document.querySelector('.city-selection-list-input-image');
-// let dropdownlist = document.querySelector('.city-selection-list-option');
-// var scrollmap = document.querySelector('.main-hero-section-map');
-// var bhuneswarx=1.2;varbhuneshwary=1.2;
-// var delhix=.8; var delhiy=.55;
-// var Luckx=.9; var Lucky=.75;
-// console.log(scrollmap);
-
-// document.addEventListener('DOMContentLoaded', function () {
-
-//     setTimeout(function () {
-
-//         var scrollmap = document.querySelector('.main-hero-section-map');
-//         var scrollLeft = scrollmap.scrollWidth / 2 - window.innerWidth / 2;
-//         var scrollTop = scrollmap.scrollHeight / 2 - window.innerHeight / 2;
-//         console.log(scrollLeft)
-//     console.log(scrollTop)
-//         // Ensure the element exists 
-//         if (scrollmap) {
-//             $(scrollmap).animate(
-//                 { scrollLeft: scrollLeft, scrollTop: scrollTop },
-//                 4000, 
-//                 'linear' 
-//             );
-//         }
-//     }, 2000); // Delay
-    
-//     setTimeout(function () {
-//         var scrollmap = document.querySelector('.main-hero-section-map');
-//         var scrollLeft = scrollmap.scrollWidth / 2 - window.innerWidth / 2;
-//         var scrollTop = scrollmap.scrollHeight / 2 - window.innerHeight / 2;
-
-//         if (scrollmap) {
-        
-//             $(scrollmap).animate(
-//                 { scrollLeft: scrollLeft*.9, scrollTop: .75*scrollTop },
-//                 4000, 
-//                 'linear'
-//             );
-//         }
-//     }, 4000);
-// });
+// Easing function for slower ease-in-out
+//!!Dont change
+function easeInOutCubic(t) {
+    return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+}
